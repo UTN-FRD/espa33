8807a6ad7e994e6b23b81dfa24267e7b2c9a316b
Mario Gonzalo Pacher <gonzipacher@gmail.com>
2022-09-08 00:04:17 -0300

    Se corrige el espaciado en las tablas

--- catalog/csv_import.php
+++ catalog/csv_import.php
@@ -1,9 +1,4 @@
 <?php
-/*
-*
-*
-*
-*/
 
 #FIX JALG --- 03-2015
 #FIX poner funcion de validacion o prueba similar a el de UPLOAD MARC
@@ -60,38 +55,40 @@ else {
 <h1><?php echo $loc->getText('CSVImportHeader'); ?></h1>
 <p><?php echo $loc->getText('CSVImportSizeLimitNotes'); ?></p>
 <form method="post" enctype="multipart/form-data" action="<?php echo $_SERVER["SCRIPT_NAME"]; ?>">
-  <label for="upload"><p><?php echo $loc->getText('CSVLabel'); ?></p></label>
-  <input type="file" name="upload" />
-  <input type="hidden" name="MAX_FILE_SIZE" value="<?php echo OBIB_MAX_FILE_SIZE; //JALG 3-2015?>"/>
+    <label for="upload">
+        <p><?php echo $loc->getText('CSVLabel'); ?></p>
+    </label>
+    <input type="file" name="upload" />
+    <input type="hidden" name="MAX_FILE_SIZE" value="<?php echo OBIB_MAX_FILE_SIZE; //JALG 3-2015?>" />
 
-  <hr />
-  <b><?php echo $loc->getText('Defaults:'); ?></b>
-  <table border=0>
-    <tr>
-      <td><?php echo $loc->getText("biblioFieldsCollection"); ?>:</td>
-      <td><?php printSelect("collectionCd","collection_dm",$postVars); ?></td>
-    </tr>
-    <tr>
-      <td><?php echo $loc->getText("biblioFieldsMaterialTyp"); ?>:</td>
-      <td><?php printSelect("materialCd","material_type_dm",$postVars); ?></td>
-    </tr>
-    <tr>
-      <td><?php echo $loc->getText("biblioFieldsOpacFlg"); ?>:</td>
-      <td>
-        <select name="opac" id="opac">
-          <option value="Y"><?php echo $loc->getText("AnswerYes"); ?></option>
-          <option value="N" selected><?php echo $loc->getText("AnswerNo"); ?></option>
-        </select>
-      </td>
-    </tr>
-    <tr>
-      <td colspan="2">
-        <input type="hidden" name="userid" id="userid" value="<?php echo H($_SESSION["userid"])?>">
-      </td>
-    </tr>
-  </table>
+    <hr />
+    <b><?php echo $loc->getText('Defaults:'); ?></b>
+    <table style="border-collapse: separate; border-spacing: 0 15px;">
+        <tr>
+            <td><?php echo $loc->getText("biblioFieldsCollection"); ?>:</td>
+            <td><?php printSelect("collectionCd","collection_dm",$postVars); ?></td>
+        </tr>
+        <tr>
+            <td><?php echo $loc->getText("biblioFieldsMaterialTyp"); ?>:</td>
+            <td><?php printSelect("materialCd","material_type_dm",$postVars); ?></td>
+        </tr>
+        <tr>
+            <td><?php echo $loc->getText("biblioFieldsOpacFlg"); ?>:</td>
+            <td>
+                <select name="opac" id="opac">
+                    <option value="Y"><?php echo $loc->getText("AnswerYes"); ?></option>
+                    <option value="N" selected><?php echo $loc->getText("AnswerNo"); ?></option>
+                </select>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <input type="hidden" name="userid" id="userid" value="<?php echo H($_SESSION["userid"])?>">
+            </td>
+        </tr>
+    </table>
 
-  <input type="submit" name="submit" class="button" value="<?php echo $loc->getText('UploadFile'); ?>" />
+    <input type="submit" name="submit" class="button" value="<?php echo $loc->getText('UploadFile'); ?>" />
 </form>
 
 <?php
